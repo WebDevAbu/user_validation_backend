3d4de52ab9774644fe15025c1e19b7824ae414bd
Torab <abutorabkhan786@gmail.com>
2023-01-04 18:36:43 +0530

    second commit

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
+require("dotenv").config();
 const express = require("express");
-// const mysql = require("mysql");/
 const cors = require("cors");
 const connection = require("./connection/Connection.js");
 const user = require("./route/userRoute.js");
 
 const app = express();
-
+const port = 5000 || process.env.PORT;
 app.use(express.json());
 app.use(cors());
 
@@ -15,6 +15,6 @@ app.get("/", (req, res) => {
 
 app.use(user);
 
-app.listen(5000, (req, res) => {
-  console.log("app is running at http://localhost:5000");
+app.listen(port, (req, res) => {
+  console.log(`app is running at http://localhost:${port}`);
 });
